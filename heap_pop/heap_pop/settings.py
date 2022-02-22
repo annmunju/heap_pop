30f7146ed862bcd62190bf121cbe39b7ceefc01d
cshun1006 <fkdepqb1006@gmail.com>
2022-02-23 02:40:07 +0900

    seongheon.html 및 그래프 png 추가

--- heap_pop/heap_pop/settings.py
+++ heap_pop/heap_pop/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -122,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]

eb4833335327ab496a726251e28d5fe187c84269
gyocheol <kys04171@naver.com>
2022-02-23 14:22:00 +0900

    gyocheol_second

--- heap_pop/heap_pop/views.py
+++ heap_pop/heap_pop/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
 
-# import pandas as pd  #성헌 시작
-# import numpy as np
-# from matplotlib import pyplot as plt
-# import seaborn as sns
-# from datetime import datetime
-# from django.shortcuts import render
-# # 필요한 패키지와 라이브러리를 가져옴
-# import matplotlib as mpl
-# import matplotlib.font_manager as fm
+import pandas as pd  #성헌 시작
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from datetime import datetime
+from django.shortcuts import render
+# 필요한 패키지와 라이브러리를 가져옴
+import matplotlib as mpl
+import matplotlib.font_manager as fm
 
-# plt.rc("font", family = "Malgun Gothic")
-# sns.set(font="Malgun Gothic",
-# rc={"axes.unicode_minus":False}, style='darkgrid') # 성헌 끝
+plt.rc("font", family = "Malgun Gothic")
+sns.set(font="Malgun Gothic",
+rc={"axes.unicode_minus":False}, style='darkgrid') # 성헌 끝
 
 
 def index(request):
@@ -24,5 +24,5 @@ def seongheon(request):
 def time_week_sub(request):
     return render(request, 'time_week_sub.html')
 
-def covid_dif_map(request):
-    return render(request, 'covid_dif_map.html')
+def gyocheol(request):
+    return render(request, 'gyocheol.html')
